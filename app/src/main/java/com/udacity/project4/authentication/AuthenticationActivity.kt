734b591dd3d1e9f4ad3b2987e1cb4faaef3ac88a
Liana Haque <lianahaque@Lianas-Pro.attlocal.net>
2022-01-10 20:27:24 -0800

    start authentication work

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,19 +1,22 @@
 package com.udacity.project4.authentication
 
-import android.nfc.Tag
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
 
-/**
- * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
- * signed in users to the RemindersActivity.
- */
 class AuthenticationActivity : AppCompatActivity() {
 
+    private val viewModel by viewModels<AuthenticationViewModel>()
     private lateinit var binding: ActivityAuthenticationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,22 +30,13 @@ class AuthenticationActivity : AppCompatActivity() {
 
 //          TODO: If the user was authenticated, send him to RemindersActivity
 
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
-
     }
 
     private fun launchSignInFlow() {
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
-
-            // This is where you can provide more ways for users to register and
-            // sign in.
         )
 
-        // Create and launch sign-in intent.
-        // We listen to the response of this activity with the
-        // SIGN_IN_REQUEST_CODE
         startActivityForResult(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
@@ -52,8 +46,38 @@ class AuthenticationActivity : AppCompatActivity() {
         )
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            } else {
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+
+    private fun observeAuthenticationState() {
+        viewModel.authenticationState.observe(this, Observer { authenticationState ->
+            when (authenticationState) {
+
+                AuthenticationViewModel.AuthenticationState.AUTHENTICATED -> {
+                    // Change UI - Navigate to appropariate screen
+                }
+
+                else -> {
+
+                    binding.loginButton.setOnClickListener {
+                    // launch to appropriate screen (launch sign in flow)
+                    }
+                }
+            }
+        })
+    }
+
     companion object {
-        const val TAG = "MainFragment"
+        const val TAG = "AuthenticationActivity"
         const val SIGN_IN_REQUEST_CODE = 1001
     }
 
