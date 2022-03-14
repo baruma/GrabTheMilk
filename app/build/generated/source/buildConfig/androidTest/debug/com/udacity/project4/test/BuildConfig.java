8d99dedca081597c478f494fcc6e9da6875c85ce
Liana Haque <lianahaque@lianas-pro.lan>
2022-03-13 21:41:21 -0700

    I'm done

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/udacity/project4/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.udacity.project4.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.udacity.project4.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String GOOGLE_MAP_KEY = "AIzaSyCfQgd752YNbd72dOovld2mYIlqima3id4";
+}
